50193a74efb5fd067a9d81f578eed678521142b7
Kyle Wright <kylejamwright@gmail.com>
2020-08-14 09:46:47 -0600

    Made the home page more mobile-friendly

--- presentation/src/App.js
+++ presentation/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Home />
         </Route>
         <Route path='/glossary/'>
-          <Glossary />
+          <Glossary  />
         </Route>
         </Switch>
     </Router>
--- presentation/src/components/home.js
+++ presentation/src/components/home.js
@@ -14,7 +14,7 @@ function App() {
         <div className='container-fluid padding'>
           <div className='row welcome text-center'>
               <div className='col-12'>
-                  <h1 className='display-3'>Web Development Glossary.</h1>
+                  <h1 className='display-3 main-title'>Web Development Glossary.</h1>
               </div>
               <hr/>
               <div className='col-12'>
@@ -24,17 +24,17 @@ function App() {
         </div>
         <div className='container-fluid padding'>
         <div className='row text-center padding'>
-        <div className='col-md-4 col-sm-6'>
+        <div className='col-lg-4 col-md-6 col-sm-6 extra-padding'>
           <h3>SEARCH</h3>
             <p>Have a word in mind? Search for it in the glossary here.</p>
             <Button color="info" href="/glossary/">Search</Button>
         </div>
-        <div className='col-md-4 col-sm-6'>
+        <div className='col-lg-4 col-md-6 col-sm-6 extra-padding'>
             <h3>POPULARITY</h3>
             <p>My algorithm finds the most relevant words/phrases for you.</p>
             <Button color="info" href="/glossary/">Popularity</Button>
         </div>
-        <div className='col-md-4 col-sm-12'>
+        <div className='col-lg-4 col-md-12 col-sm-12 extra-padding'>
             <h3>TOPICS</h3>
             <p>Want to be an expert in a specific topic? Start here.</p>
             <Button color="info" href="/glossary/">Topic</Button>
