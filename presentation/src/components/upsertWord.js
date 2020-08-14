96ee98092e2459e4ec349ff067af6fc71d8d685e
Kyle Wright <kylejamwright@gmail.com>
2020-08-13 19:36:49 -0600

    Built functionality to update the meta data on a term

--- presentation/src/components/upsertWord.js
+++ presentation/src/components/upsertWord.js
@@ -3,16 +3,10 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input, Form, FormGr
 
 const UpsertWord = (props) => {
   const [modal, setModal] = useState(false);
-  const [word, setWord] = useState(async() => {
-    // await fetch();
-    return {...props.word}
+  const [word, setWord] = useState(async () => {
+    return {term: "", definition: [""], visits: 0, meta: [], archived: false}
   })
-  const [frontEnd, setFrontEnd] = useState(() => {
-    if (props.word !== undefined && props.word.meta.findIndex(ele => ele === 'front-end') !== -1) {
-      return true;
-    }
-    return false;
-  });
+  const [frontEnd, setFrontEnd] = useState(() => false);
   const [backEnd, setBackEnd] = useState(() => false);
   const [style, setStyle] = useState(() => false);
   const [data, setData] = useState(() => false);
@@ -34,50 +28,88 @@ const UpsertWord = (props) => {
     setData(!prev);
   }
 
-    const toggle = () => setModal(!modal);
+  const toggle = async () => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/glossary/${props.id}`);
+    const data = await response.json();
+    data._id = props.id;
+    setWord(data);
+    if (data.meta.findIndex(ele => ele === 'front-end') !== -1) {setFrontEnd(true)}
+    else {setFrontEnd(false)}
+    if (data.meta.findIndex(ele => ele === 'back-end') !== -1) {setBackEnd(true)}
+    else {setBackEnd(false)}
+    if (data.meta.findIndex(ele => ele === 'style') !== -1) {setStyle(true)}
+    else {setStyle(false)}
+    if (data.meta.findIndex(ele => ele === 'data') !== -1) {setData(true)}
+    else {setData(false)}
+    setModal(!modal)
+  };
 
-    const handleClickUpdate = async () => {
-        const data = {...word};
-        delete data._id;
-        const options = {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }
-        await fetch(`${process.env.REACT_APP_API_URL}/glossary/${word._id}`, options);
-        toggle();
-        props.refresh();
-    }
-  
-    const addDefinition = () => {
+  const handleClickUpdate = async () => {
       const obj = {...word};
-      obj.definition.push('');
-      setWord(obj);
-    }
-    const removeDefinition = () => {
-      const obj = {...word};
-      obj.definition.pop();
-      setWord(obj);
-    }
-  
-    const handleChange = (event) => {
-      const obj = {...word};
-      obj[event.target.name] = event.target.value;
-      setWord(obj);
-    }
-    const handleChangeDef = (event, index) => {
-      const obj = {...word};
-      obj.definition[index] = event.target.value;
-      setWord(obj);
-    }
+      if (frontEnd && obj.meta.findIndex(ele => ele === 'front-end') === -1) {obj.meta.push('front-end')}
+      else if (!frontEnd) {
+        const index = obj.meta.findIndex(ele => ele === 'front-end');
+        if (index !== -1) {obj.meta.splice(index, 1)}
+      }
 
-    let definitions = [''];
-    word.term !== undefined ? definitions = word.definition.map((def, index) => {
-        return <Input style={{marginBottom: "10px", height: "auto"}} type="textarea" name="definition" id="definition" value={def} onChange={event => handleChangeDef(event, index)} />
-      }) : definitions = [''];
+      if (backEnd && obj.meta.findIndex(ele => ele === 'back-end') === -1) {obj.meta.push('back-end')}
+      else if (!backEnd) {
+        const index = obj.meta.findIndex(ele => ele === 'back-end');
+        if (index !== -1) {obj.meta.splice(index, 1)}
+      }
+
+      if (style && obj.meta.findIndex(ele => ele === 'style') === -1) {obj.meta.push('style')}
+      else if (!style) {
+        const index = obj.meta.findIndex(ele => ele === 'style');
+        if (index !== -1) {obj.meta.splice(index, 1)}
+      }
+
+      if (data && obj.meta.findIndex(ele => ele === 'data') === -1) {obj.meta.push('data')}
+      else if (!data) {
+        const index = obj.meta.findIndex(ele => ele === 'data');
+        if (index !== -1) {obj.meta.splice(index, 1)}
+      }
+
+      delete obj._id;
+      const options = {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(obj)
+      }
+      await fetch(`${process.env.REACT_APP_API_URL}/glossary/${props.id}`, options);
+      toggle();
+      props.refresh();
+  }
   
+  const addDefinition = () => {
+    const obj = {...word};
+    obj.definition.push('');
+    setWord(obj);
+  }
+  const removeDefinition = () => {
+    const obj = {...word};
+    obj.definition.pop();
+    setWord(obj);
+  }
+  
+  const handleChange = (event) => {
+    const obj = {...word};
+    obj[event.target.name] = event.target.value;
+    setWord(obj);
+  }
+  const handleChangeDef = (event, index) => {
+    const obj = {...word};
+    obj.definition[index] = event.target.value;
+    setWord(obj);
+  }
+
+  let definitions = [''];
+  word.term !== undefined ? definitions = word.definition.map((def, index) => {
+      return <Input style={{marginBottom: "10px", height: "auto"}} type="textarea" name="definition" id="definition" value={def} onChange={event => handleChangeDef(event, index)} />
+    }) : definitions = [''];
+
     return (
       <span>
         <Button outline color="secondary"  style={{marginLeft: "10px", marginTop: "10px"}} onClick={toggle}><i className="fa fa-gear" style={{fontSize:"20px"}}></i></Button>
