315a1b972daf78ba942e3e0240097a9c76b4b33f
Kyle Wright <kylejamwright@gmail.com>
2020-08-14 09:21:45 -0600

    Fixed the routing problem in the App.js file

---
+++ public/precache-manifest.ed0d73c2066bf899c1644c059b564c1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2d06d091328c777e5e126524b704c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c23f02b8eedc64a77893",
+    "url": "/static/css/2.98f886d0.chunk.css"
+  },
+  {
+    "revision": "65255972a67e7fb11c79",
+    "url": "/static/css/main.af9e40c0.chunk.css"
+  },
+  {
+    "revision": "c23f02b8eedc64a77893",
+    "url": "/static/js/2.1d3e1726.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.1d3e1726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65255972a67e7fb11c79",
+    "url": "/static/js/main.d424fc76.chunk.js"
+  },
+  {
+    "revision": "1aadcde5ef86e7287682",
+    "url": "/static/js/runtime-main.44d2f03b.js"
+  },
+  {
+    "revision": "f2f223c0e90f8240507281284489c619",
+    "url": "/static/media/IconLight.f2f223c0.png"
+  }
+]);
